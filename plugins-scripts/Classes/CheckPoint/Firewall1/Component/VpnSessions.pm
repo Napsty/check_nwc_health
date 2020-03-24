634d0443fdc1bc3b9d8115973665ab37183ef239
Napsty <ck@claudiokuenzler.com>
2020-03-24 14:56:07 +0100

    Add vpn sessions for CP FW1

---
+++ plugins-scripts/Classes/CheckPoint/Firewall1/Component/VpnSessions.pm
@@ -0,0 +1,21 @@
+package Classes::CheckPoint::Firewall1::Component::VpnSessions;
+our @ISA = qw(Monitoring::GLPlugin::SNMP::Item);
+use strict;
+
+sub init {
+  my ($self) = @_;
+  $self->get_snmp_objects('CHECKPOINT-MIB', (qw(cpvIKECurrRespSAs cpvIKEMaxConncurRespSAs)));
+}
+
+sub check {
+  my ($self) = @_;
+  $self->add_info(sprintf '%.2f vpn sessions used', $self->{cpvIKECurrRespSAs});
+  $self->set_thresholds(warning => 180, critical => 200);
+  $self->add_message($self->check_thresholds($self->{cpvIKECurrRespSAs}));
+  $self->add_perfdata(
+      label => 'vpn_sessions',
+      value => $self->{cpvIKECurrRespSAs},
+      min => 0,
+      max => $self->{cpvIKEMaxConncurRespSAs},
+  );
+}
